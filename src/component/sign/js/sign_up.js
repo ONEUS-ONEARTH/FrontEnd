df6c9da6d706139bf575c8168866809fbf90364c
GaRyung <shubom0115@naver.com>
2024-10-12 20:51:05 +0900

    signup 만드는중

--- src/component/sign/js/sign_up.js
+++ src/component/sign/js/sign_up.js
@@ -1,39 +1,112 @@
 import React from "react";
 import Header from "../../header/js/header";
-import { useState } from 'react-router-dom';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import "../scss/sign-up.scss"
 
 
 const Sign_up = () => {
+    const redirection = useNavigate(); // 리다이렉트 함수를 리턴
 
-    const [userValue, setUserValue] = useState({
+    // 상태변수로 회원가입 입력값 관리
+    const[userValue, setUserValue] = useState({
         name:'',
         nickname:'',
-        password:''
+        email:'',
+        password:'',
+        adress:'',
+        phoneNumber:''
     });
 
     // 입력값 검증 메시지를 관리할 상태변수
     const [message, setMessage] = useState({
         password: '',
         passwordCheck: '',
-        id: ''
+        email: ''
     });
 
     // 검증 완료 체크에 대한 상태변수 관리
     const [correct, setCorrect] = useState({
-        userName: false,
         password: false,
         passwordCheck: false,
-        id: false
+        email: false
     });
 
 
-    nameHandler = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value, // <- 변경 후
+    const saveInputState = (flag, msg, inputVal, key) => {
+
+        setCorrect({
+            ...correct,
+            [key]: flag
         });
-        setUserValue({...userValue, userName: inputVal});
+
+        setMessage({
+            ...message,
+            [key]: msg
+        });
+
+        setUserValue({
+            ...userValue,
+            [key]: inputVal
+        });
+
     };
+
+
+
+    const nameHandler = (e) => {
+        const inputVal = e.target.value;
+        setUserValue({...userValue, name:inputVal});
+    }
+    const nicknameHandler = (e) => {
+        const inputVal = e.target.value;
+        setUserValue({...userValue, name:inputVal});
+    }
+
+    // 이메일 입력값을 검증하고 관리할 함수
+    const emailHandler = e => {
+        const inputVal = e.target.value;
+
+        const emailRegex = /^[a-z0-9\.\-_]+@([a-z0-9\-]+\.)+[a-z]{2,6}$/;
+
+        let msg, flag;
+        if (!inputVal) {
+            msg = '이메일은 필수값입니다!';
+            flag = false;
+        } else if (!emailRegex.test(inputVal)) {
+            msg = '이메일 형식이 아닙니다!';
+            flag = false;
+        } else {
+            // 이메일 중복체크
+            fetchDuplicatedCheck(inputVal);
+            return;
+        }
+
+        saveInputState(flag, msg, inputVal, 'email');
+    };
+
+    // 이메일 중복체크
+    const fetchDuplicatedCheck = async (id) => {
+
+        // let msg = '', flag = false;
+        //
+        // const res = await fetch( API_BASE_URL + "/check?id=" + id);
+        // const json = await res.json();
+        //
+        // if (json) {
+        //     msg = '이메일이 중복되었습니다!';
+        //     flag = false;
+        // } else {
+        //     msg = '사용 가능한 이메일입니다.';
+        //     flag = true;
+        // }
+        // setUserValue({...userValue, id: id});
+        // setMessage({...message, id: msg});
+        // setCorrect({...correct, id: flag});
+    };
+
+
+
     return (
         <>
             <Header/>
@@ -42,50 +115,24 @@ const Sign_up = () => {
                     <p>회원가입</p>
                 </div>
                 <form className="signup-box" action="">
-                    <div className="nn-box">
-                        <div className="name-box">
-                            <p>이름</p>
-                            <input className="nm-input input-h" type="text" name="name" onChange={nameHandler}/>
+                    <div className="profile-box">
+
+                    </div>
+                    <div className="sign-info-box">
+                        <div className="nn-box">
+                            <input className="name-box nn-input" type="text" name="name" onChange={nameHandler} placeholder="이름"/>
+                            <input className="nickname-box nn-input" type="text" name="nickname" onChange={nicknameHandler} placeholder="별명"/>
                         </div>
-                        <div className="nickname-box">
-                            <p>별명</p>
-                            <input className="nm-input input-h" type="text" name="nickname" onChange={nameHandler}/>
-                        </div>
-                    </div>
-                    <div className="id-box info-box">
-                        <p>&nbsp;&nbsp;&nbsp;아이디</p>
-                        <input className="text-box input-h" type="text" name='email' placeholder="이메일을 적어주세요"/>
-                    </div>
-                    <div className="pw-box info-box">
-                        <p>비밀번호</p>
-                        <input className="text-box input-h" type="password" name=''/>
-                    </div>
-                    <div className="pwck-box info-box">
-                        <p>번호확인</p>
-                        <input className="text-box input-h" type="password"/>
-                    </div>
-                    <div className="pn-box">
-                        <p>전화번호</p>
-                        <div className="pn-text-box">
-                            <select name="" id="pn-select">
-                                <option value="010">010</option>
-                                <option value="011">011</option>
-                                <option value="012">012</option>
-                            </select>
-                            <p>-</p>
-                            <input className="pn-input-box input-h" type="text"/>
-                            <p>-</p>
-                            <input className="pn-input-box input-h" type="text"/>
+                        <div className="beside-inputs">
+                            <input className="id-box low-input" type="text" name="email" onChange={emailHandler} placeholder="아이디 ex)abcdef@gmail.com"/>
+                            <input className="pw-box low-input" type="password" placeholder="비밀번호"/>
+                            <input className="pw-ck-box low-input" type="password" placeholder="비밀번호다시확인"/>
+                            <input className="ph-box low-input" type="text" placeholder="전화번호"/>
+                            <input className="ad-box low-input" type="text" placeholder="주소"/>
                         </div>
                     </div>
-                    <div className="ad-box info-box">
-                        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;주소</p>
-                        <input className="text-box input-h" type="text" name="adress"/>
-                    </div>
-                    <div className="submit-box">
-                        <div className="signup-btn">
-                            <p>회원가입</p>
-                        </div>
+                    <div className="signup-btn">
+                        <p>회원가입</p>
                     </div>
                 </form>
             </div>
