6362f6c6f1b1f5755425292f0f5bd6a99b6815f3
GaRyung <shubom0115@naver.com>
2024-11-01 14:36:37 +0900

    mypage 정보 불러오는중

--- src/component/mypage/js/my_profile.js
+++ src/component/mypage/js/my_profile.js
@@ -1,9 +1,42 @@
-import React from "react";
+import React, {useEffect, useState} from 'react';
 import "../scss/my_profile.scss"
 import Header from "../../header/js/header";
 import My_info_header from "./my_info_header";
+import {USER_URL} from "../../../config/host-config";
 
 const My_profile = () => {
+    const PROFILE_URL = USER_URL + "/myprofile";
+    const MODIFY_URL = USER_URL + "/modify";
+    const [userProfile, setUserProfile] = useState({
+        name: '',
+        nickname: '',
+        email: '',
+        password: '',
+        phoneNumber: '',
+        adress: '',
+        imagePath: ''
+    });
+
+    const storedToken = localStorage.getItem('ACCESS_TOKEN');
+    useEffect(() => {
+        // 로그인 상태 확인 로직 (localStorage 또는 서버 API 호출)
+
+        userProfileFetch();
+    },[]);
+
+    const userProfileFetch = async () =>{
+        const url = PROFILE_URL
+        const res = await fetch({
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${storedToken}`
+            }
+        });
+        if (res.status === 200) {
+           
+        }
+
+    }
 
     return (
         <>
